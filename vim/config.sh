28489e47d0fd224dca34663be018ad63637aa50a
zzqboy <ziquanzheng@foxmail.com>
2018-05-30 15:11:11 -0700

    补全插件todo

--- vim/config.sh
+++ vim/config.sh
@@ -29,14 +29,4 @@ mkdir ~/.vim/colors/
 git clone git@github.com:tomasr/molokai.git molokai
 cp molokai/colors/molokai.vim  ~/.vim/colors/
 
-# you complete me
-#git clone git@github.com:Valloric/YouCompleteMe.git YouCompleteMe
-#cp -r YouCompleteMe  ~/.vim/bundle/YouCompleteMe
-#mkdir ~/.ycm_build
-#cd ~/.ycm_build
-#cmake -G "Unix Makefiles" -DUSE_SYSTEM_BOOST=ON -DEXTERNAL_LIBCLANG_PATH=/usr/lib/x86_64-linux-gnu/libclang-3.9.so . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
-#cmake --build . --target ycm_core --config Release
-#cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/examples/.ycm_extra_conf.py ~/.vim/
-#cd -
-
 cp -f .vimrc ~/.vimrc
